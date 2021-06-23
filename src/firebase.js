6ba20ff3125748d95b1a26e98d2dcdc61a046006
pallavi002 <pallavilodhi08@gmail.com>
2021-06-23 21:33:16 +0530

    firebase changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBvDqE5H_IJyc6wGxPBh4JEoIn43y7sbwk",
-    authDomain: "internship-shopping.firebaseapp.com",
-    projectId: "internship-shopping",
-    storageBucket: "internship-shopping.appspot.com",
-    messagingSenderId: "13772036395",
-    appId: "1:13772036395:web:e2798d996526637a328d6e",
-    measurementId: "G-ZEN05YFMW7"
-  };
+  apiKey: "AIzaSyBvDqE5H_IJyc6wGxPBh4JEoIn43y7sbwk",
+  authDomain: "internship-shopping.firebaseapp.com",
+  projectId: "internship-shopping",
+  storageBucket: "internship-shopping.appspot.com",
+  messagingSenderId: "13772036395",
+  appId: "1:13772036395:web:e2798d996526637a328d6e",
+  measurementId: "G-ZEN05YFMW7"
+};
 
 firebase.initializeApp(firebaseConfig);
 
